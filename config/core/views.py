39acacd91419bdf73b9a88bc4ccf661ec51cbce0
Olaf <oli.madryt@gmail.com>
2025-01-31 09:55:40 +0100

    Add core app with user login functionality and update project settings

--- config/core/views.py
+++ config/core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("You are logged in.")
+        else:
+            return HttpResponse("Invalid login details.")
+    else:
+        return render(request, 'login.html')
